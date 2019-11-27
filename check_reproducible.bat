313d02bf1f9378ca0cc630331fee2b404248ee7b
eksperimental <eksperimental@users.noreply.github.com>
2019-11-27 09:05:56 +0700

    .bat

--- check_reproducible.bat
+++ check_reproducible.bat
@@ -1,11 +1,11 @@
 @if defined ELIXIR_CLI_ECHO (@echo on) else (@echo off)
 
-echo Diffing...
-fc lib/elixir/ebin/* lib/elixir/tmp/ebin_reproducible/*
-fc lib/eex/ebin/* lib/eex/tmp/ebin_reproducible/*
-fc lib/ex_unit/ebin/* lib/ex_unit/tmp/ebin_reproducible/*
-fc lib/iex/ebin/* lib/iex/tmp/ebin_reproducible/*
-fc lib/logger/ebin/* lib/logger/tmp/ebin_reproducible/*
-fc lib/mix/ebin/* lib/mix/tmp/ebin_reproducible/*
+echo Diffing (Windows) ...
+fc lib/elixir/ebin/* lib/elixir/tmp/ebin_reproducible/* &&
+fc lib/eex/ebin/* lib/eex/tmp/ebin_reproducible/* &&
+fc lib/ex_unit/ebin/* lib/ex_unit/tmp/ebin_reproducible/* &&
+fc lib/iex/ebin/* lib/iex/tmp/ebin_reproducible/* &&
+fc lib/logger/ebin/* lib/logger/tmp/ebin_reproducible/* &&
+fc lib/mix/ebin/* lib/elixir/tmp/ebin_reproducible/* || exit /b
 echo Builds are reproducible
-echo.
+echo ""
