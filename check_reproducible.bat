6888229879e0a4f5b20d5c6decbb62c5f89ece0c
eksperimental <eksperimental@users.noreply.github.com>
2019-11-27 10:10:20 +0700

    fix

--- check_reproducible.bat
+++ check_reproducible.bat
@@ -1,6 +1,6 @@
 @if defined ELIXIR_CLI_ECHO (@echo on) else (@echo off)
 
-echo Diffing (Windows) ...
+echo Diffing -Windows- ...
 fc lib/elixir/ebin/* lib/elixir/tmp/ebin_reproducible/* &&
 fc lib/eex/ebin/* lib/eex/tmp/ebin_reproducible/* &&
 fc lib/ex_unit/ebin/* lib/ex_unit/tmp/ebin_reproducible/* &&
